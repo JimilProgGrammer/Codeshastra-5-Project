1095e37c524e1a7d16387018f68d386f28571b50
ruchii <shuklaruchi1063@gmail.com>
2019-03-03 00:39:01 +0530

    added profile body

--- portfolio/src/app/app.routing.ts
+++ portfolio/src/app/app.routing.ts
@@ -1,4 +1,5 @@
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent} from './profile/profile.component';
 import { SignupComponent } from './signup/signup.component';
 import { Routes, RouterModule, Router } from '@angular/router';
 import { NgModule } from '@angular/core';
@@ -6,7 +7,8 @@ import { NgModule } from '@angular/core';
 const routes: Routes = [
     { path:'', pathMatch: 'full', redirectTo: 'home' },
     { path:'signup', component: SignupComponent },
-    { path:'home', component: HomeComponent }
+    { path:'home', component: HomeComponent },
+    { path:'profile', component: ProfileComponent }
 ]
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
